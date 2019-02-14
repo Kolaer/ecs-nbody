1de7bc228a5b811a3b1d00dc21eb84b58e2472f6
Kolaer <overlordin777@gmail.com>
2019-02-14 14:56:09 +0400

    Added simple sdl2 based rendering.
    Signed-off-by: Kolaer <overlordin777@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 extern crate rand;
 extern crate rayon;
+extern crate sdl2;
 extern crate specs;
 
 use rand::prelude::*;
 use rayon::prelude::*;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
 use specs::prelude::*;
-
-use std::time::Instant;
 
 const GRAVITY_CONST: f32 = 1e-5;
 const NUM_OBJ: u32 = 1_000;
-const NUM_ITER: u32 = 60;
 
 #[derive(Default)]
 struct DeltaTime(f32);
@@ -89,6 +91,11 @@ impl<'a> System<'a> for UpdateVel {
                         if first_ent.id() != second_ent.id() {
                             let r_sq = (second_pos.x - first_pos.x).powi(2)
                                 + (second_pos.y - first_pos.y).powi(2);
+
+                            if r_sq >= 1e6 {
+                                return;
+                            }
+
                             let force = GRAVITY_CONST * second_mass / r_sq;
 
                             let dir_x_sq = (second_pos.x - first_pos.x).powi(2);
@@ -98,6 +105,18 @@ impl<'a> System<'a> for UpdateVel {
 
                             let dir_x = dir_x_sq / dir_mag_sq;
                             let dir_y = dir_y_sq / dir_mag_sq;
+
+                            let dir_x = if second_pos.x > first_pos.x {
+                                dir_x
+                            } else {
+                                -dir_x
+                            };
+
+                            let dir_y = if second_pos.y > first_pos.y {
+                                dir_y
+                            } else {
+                                -dir_y
+                            };
 
                             let acc_x = force * dir_x;
                             let acc_y = force * dir_y;
@@ -139,41 +158,66 @@ fn main() {
 
     world.add_resource(DeltaTime(0.1));
 
-    let now = Instant::now();
-
-    println!("Number of objects: {}", NUM_OBJ);
-    println!("Number of simulation steps: {}", NUM_ITER);
-
     for _ in 0..NUM_OBJ {
         world
             .create_entity()
             .with(Position {
-                x: random::<f32>(),
-                y: random::<f32>(),
+                x: 20.0 * random::<f32>(),
+                y: 20.0 * random::<f32>(),
             })
-            .with(Velocity {
-                x: random::<f32>(),
-                y: random::<f32>(),
-            })
-            .with(Mass(random::<f32>().abs() + 1e-5))
+            .with(Velocity { x: 0.0, y: 0.0 })
+            .with(Mass(1.0))
             .build();
     }
-
-    println!("Init time: {:?}", now.elapsed());
 
     let mut dispatcher = DispatcherBuilder::new()
         .with(UpdateVel, "update_vel", &[])
         .with(UpdatePos, "update_pos", &["update_vel"])
         .build();
 
-    let now = Instant::now();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-    for _ in 0..NUM_ITER {
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 512, 512)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(5, 5, 5));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        canvas.set_draw_color(Color::RGB(5, 5, 5));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
         dispatcher.dispatch(&world.res);
+        world.maintain();
+
+        let pos_storage = world.read_storage::<Position>();
+        let mass_storage = world.read_storage::<Mass>();
+
+        canvas.set_draw_color(Color::RGB(240, 240, 240));
+        for (pos, _mass) in (&pos_storage, &mass_storage).join() {
+            let x = (pos.x / 20.0 - 0.5) * 128.0 + 256.0;
+            let y = (pos.y / 20.0 - 0.5) * 128.0 + 256.0;
+            let p = Point::new(x.round() as i32, y.round() as i32);
+            canvas.draw_point(p).unwrap();
+        }
+
+        canvas.present();
     }
-
-    let elapsed = now.elapsed();
-
-    println!("Total simulation time: {:?}", elapsed);
-    println!("Mean time of simulation step: {:?}", elapsed / NUM_ITER);
 }
